57533a362bd0ad3d66fbe3f3c125ac74a1e78025
Michael G. H. Larsen <mike.gh.larsen@gmail.com>
2010-03-25 23:54:31 -0400

    route.py: Parse CLI args correctly.

--- route.py
+++ route.py
@@ -31,4 +31,4 @@ if __name__ == '__main__':
     if len(sys.argv) < 3:
 	print(usage)
     else:
-	print(routeFind(int(sys.argv[1], sys.argv[2])))
+	print(routeFind(int(sys.argv[1]), int(sys.argv[2])))
